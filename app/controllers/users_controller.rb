5f05b6b146c8accc99c732da8d32f391f72e596a
Praveen R. Shanbhag <shanbhagp@aol.com>
2012-07-12 19:17:10 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+before_filter :correct_user,   only: [:edit, :update]
+before_filter :admin_user,     only: :destroy
+
  def show
     @user = User.find(params[:id])
   end
@@ -11,7 +16,9 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save  #???this is what saves the user upon clicking the submit button (in the
       # 'new' page and thereby submitting a post request (executing the create action)
-      # - that is, @user.save is a command? which if successful returns true?
+      # - that is, @user.save is a command? which if successful returns true?  or is 
+      # the user automatically saved by the post request (if validations pass)?  
+      # it seems like (From section 9.1.2) that the if @user.save actually attempst the save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
@@ -20,5 +27,52 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+
+  def edit
+   # @user = User.find(params[:id])
+  end
+
+  def update
+     # @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end 
+
+  def index
+    #@users = User.all
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
+private
+
+   def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
 
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,22 +28,104 @@ describe "Authentication" do
 
       describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-       before do
-        fill_in "Email",    with: user.email
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-       end
-
+       #before do
+        #fill_in "Email",    with: user.email
+        #fill_in "Password", with: user.password
+        #click_button "Sign in"
+        before { sign_in user }
+       #end
+      it { should have_link('Users',    href: users_path) }
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+        describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+        end
+
       end #end "with valid information" description
 
    
    end #end "signin" description#
 
 
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+        # before {visit edit_user_path(user)} #
+        ##describe "visiting the edit page" do
+         ## before { visit edit_user_path(user) }
+         ## end  #was I supposed to comment ##lines out?  did so for 9.17, implementing
+         #visiting protected page stuff below
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          # before {click_buttion "Save changes" } #  #why doesn't click_button work? b/c no page
+          # specified when button clicks yet?
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+      end #for in the users controller
+
+        describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+        end 
+      end # for visiting a protected page
+
+
+    end #for non-signed in users
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user }
+
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_selector('title', text: full_title('Edit user')) }
+      end
+
+      describe "submitting a PUT request to the Users#update action" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }        
+      end
+    end
+
+  end #end authorization
 
 
  end
